c48dc2b2aae1f318fe6c84e7ed6491002b992d97
moog.kwon <diligentaf@gmail.com>
2022-08-10 10:43:48 +0900

    chapter 5 done

--- struct_5/src/main.rs
+++ struct_5/src/main.rs
@@ -1,9 +1,36 @@
 struct User {
     active: bool,
-    username: String,
+    username: String, // &str won't work. Unless you use "Rust's lifetime"
     email: String,
     sign_in_count: u64,
 } // stuct uses key: value pairs
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle { // impl block are called "Associated Functions"
+    fn area(&self) -> u32 { // &self is basically an abbreviation of (rectangle : &Rectangle)
+        self.width * self.height
+    }
+
+    fn width(&self) -> bool { // &self is basically an abbreviation of (rectangle : &Rectangle)
+        self.width > 0
+    }
+
+    fn can_hold(&self, other:&Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+}
 
 fn main() {
     //  == Creating Struct ==
@@ -71,6 +98,85 @@ fn main() {
     let origin = Point(0, 0, 0);
     println!("{}", black.0);
     println!("{}", origin.0);
+
+    //  == Calculating Rectangle ==
+    println!();
+    println!("== Calculating Rectangle ==");
+    let width1 = 30;
+    let height1 = 50;
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(width1, height1)
+    );
+
+    //  == Refactoring with Tuples ==
+    println!();
+    println!("== Refactoring with Tuples ==");
+
+    let rect1 = (30, 50);
+    println!(
+        "The area of the ractangle is {} square pixels.",
+        area_tuple(rect1)
+    );
+
+    //  == Refactoring with Structs (win for clarity) ==
+    println!();
+    println!("== Refactoring with Structs (win for clarity) ==");
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area_struct(&rect1)
+    );
+
+    //  == Priniting Struct using -> #[derive(Debug)] ==
+    println!();
+    println!("== Priniting Struct using -> #[derive(Debug)] ==");
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    println!("rect1 is {:?}", rect1);
+    dbg!(&rect1); // This debugging also works!. We don't want dbg! to take ownership so used reference
+
+    //  == Defining Methods ==
+    println!();
+    println!("== Defining Methods ==");
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+    println!( "It is {}, that rectangle has a nonzero width", rect1.width());
+
+    //  == Automatic Referencing and Dereferencing ==
+    println!();
+    println!("== Automatic Referencing and Dereferencing ==");
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 80,
+        height: 90,
+    };
+    let rect3 = Rectangle {
+        width: 20,
+        height: 40,
+    };
+    println!("{}",rect1.can_hold(&rect2));
+    println!("{}",rect1.can_hold(&rect3));
+
+    //  == Constructor in Struct ==
+    println!();
+    println!("== Constructor in Struct ==");
+    dbg!(Rectangle::square(3)); // new struct instance has been created
+
 }
 
 fn build_user(email: String, username: String) -> User {
@@ -80,4 +186,16 @@ fn build_user(email: String, username: String) -> User {
         active: true,
         sign_in_count: 1,
     }
+}
+
+fn area(width: u32, height: u32) -> u32 {
+    width * height
+}
+
+fn area_tuple(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
+}
+
+fn area_struct(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
